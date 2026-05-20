aad1990d4abf7b18d4c570e1b36b8814345f9dbb
SADAB AZHAR <sadabazhar84@gmail.com>
2026-05-20 13:47:13 +0530

    chore: initialize frontend architecture

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="bg-black text-white min-h-screen flex items-center justify-center">
+        RapidLink UI
+      </div>
+    </>
+  );
+}
+
+export default App;
